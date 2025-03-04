6ee067588012e62bb96ffd5c986800ae0d04e347
lauraHerSan01 <hernandez.sandoval.laura@gmail.com>
2025-03-04 10:02:10 -0600

    13 log in modal

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -22,7 +22,7 @@ const Modal: React.FC<ModalProps>=({
 
     const handleClose = useCallback(()=>{
         setShowModal(false);
-
+        
         setTimeout(()=>{
             close();
         }, 300)
@@ -31,7 +31,6 @@ const Modal: React.FC<ModalProps>=({
     if(!isOpen){
         return null;
     }
-
     return(
         <div className="flex items-center justify-center fixed inset-0 z-50 bg-black/60">
             <div className=" relative w-[90%] md:w-[80%] lg:w-[700px] my-6 mx-auto h-auto">
@@ -39,7 +38,10 @@ const Modal: React.FC<ModalProps>=({
                     <div className="w-full h-auto rounded-xl relative flex flex-col bg-white">
                         
                         <header className="h-[60px] flex items-center p-6 rounded justify-center relative border-b">
-                            <div className="p-3 absolute left-3 hover:bg-gray-300 rounded-full cursor-pointer">
+                            <div 
+                                onClick={handleClose}
+                                className="p-3 absolute left-3 hover:bg-gray-300 rounded-full cursor-pointer"
+                            >
                                 <svg fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
                                 </svg>
--- app/components/navbar/UserNav.tsx
+++ app/components/navbar/UserNav.tsx
@@ -2,8 +2,10 @@
 
 import {useState}from "react";
 import MenuLink from "./MenuLink";
+import useLoginModal from "@/app/hooks/useLoginModal";
 
 const UserNav = () =>{
+    const loginModal = useLoginModal ();
     const [isOpen, setIsOpen] = useState(false)
 
     return(
@@ -25,7 +27,12 @@ const UserNav = () =>{
                 <div className="w-[220px] absolute top-[60px] right-0 bg-white border rounded-xl shadow-md flex flex-col cursor-pointer">
                     <MenuLink
                         label='Log in'
-                        onclick={() => console.log('Clicked button')}
+                        onclick={() => {
+                            console.log('Clicked button')
+
+                            setIsOpen(false);
+                            loginModal.open()
+                        }}
                     />
 
                         <MenuLink
