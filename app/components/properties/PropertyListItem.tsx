f16fe87c72747fabf2fa34f0b135daf2f5c0912b
lauraHerSan01 <hernandez.sandoval.laura@gmail.com>
2025-03-30 02:28:28 -0600

    24 Make it possible to set property as favorite

--- app/components/properties/PropertyListItem.tsx
+++ app/components/properties/PropertyListItem.tsx
@@ -1,13 +1,17 @@
 import Image from "next/image";
 import { PropertyType } from "./PropertyList";
 import { useRouter } from "next/navigation";
+import FavoriteButton from "../FavoriteButton";
+
 
 interface PropertyProps {
-    property: PropertyType
+    property: PropertyType,
+    markFavorite?: (is_favorite: boolean) => void;
 }
 
 const PropertyListItem: React.FC<PropertyProps> =({
-    property
+    property,
+    markFavorite,
 }) => {
     const router = useRouter();
 
@@ -26,6 +30,14 @@ const PropertyListItem: React.FC<PropertyProps> =({
                     className="hover:scale-110 object-cover transition h-full w-full"
                     alt="Beach house"
                 />
+
+                {markFavorite && (
+                    <FavoriteButton
+                        id={property.id}
+                        is_favorite={property.is_favorite}
+                        markFavorite={(is_favorite) => markFavorite(is_favorite)}
+                    />
+                )}
             </div>
 
             <div className="mt-2">
