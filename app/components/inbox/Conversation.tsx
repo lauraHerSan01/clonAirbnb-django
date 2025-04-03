267174e2b5fda437d31eb57a7db1635d63c852f1
lauraHerSan01 <hernandez.sandoval.laura@gmail.com>
2025-04-03 10:29:28 -0600

    30 Start new conversation

--- app/components/inbox/Conversation.tsx
+++ app/components/inbox/Conversation.tsx
@@ -15,7 +15,6 @@ const Conversation: React.FC<ConversationProps> = ({
 }) => {
     const router = useRouter();
     const otherUser = conversation.users.find((user) => user.id != userId)
-
     return (
         <div className="px-6 py-4 cursor-pointer border border-gray-300 rounded-xl">
             <p className="mb-6 text-xl">{otherUser?.name}</p>
